8c2d771933a711c18116cb78f5ee10303574ca30
zack6yuan <zack6yuan@gmail.com>
2025-10-17 14:01:30 -0500

    removed icons, and styling the login page.

--- app/login.tsx
+++ app/login.tsx
@@ -1,18 +1,36 @@
 import { Link, useRouter } from "expo-router";
-import { View, Text, Pressable } from "react-native";
+import { StyleSheet, View, Text, Pressable, Image } from "react-native";
 
 export default function loginPage() {
   const router = useRouter();
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Login</Text>
-      <Link href="/register" replace>
-        <Text>Create a new account</Text>
-      </Link>
+    <View style={styles.container}>
+      <Image style={styles.logo} source={require("../assets/images/logo.png")}></Image>
+      <Text style={styles.text}>Login</Text>
       <Pressable onPress={() => {router.replace("/(tabs)/");}}>
-        <Text>Sign in</Text>
+        <Text style={styles.text}>Sign in</Text>
       </Pressable>
+      <Link href="/register" replace>
+        <Text style={styles.text}>Create a new account</Text>
+      </Link>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#000435',
+  },
+  logo: {
+    resizeMode: 'contain',
+    width: 250,
+    height: 200,
+  },
+  text: {
+    color: 'white',
+  }
+})
