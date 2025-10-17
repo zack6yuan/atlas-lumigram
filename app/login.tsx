f648107b17d3ac54b927c67114ecab004e90d2e5
zack6yuan <zack6yuan@gmail.com>
2025-10-17 18:45:12 -0500

    about finished with the login page.

--- app/login.tsx
+++ app/login.tsx
@@ -1,18 +1,28 @@
 import { Link, useRouter } from "expo-router";
-import { StyleSheet, View, Text, Pressable, Image } from "react-native";
+import { StyleSheet, View, Text, Pressable, Image, TextInput } from "react-native";
 
 export default function loginPage() {
   const router = useRouter();
 
   return (
     <View style={styles.container}>
-      <Image style={styles.logo} source={require("../assets/images/logo.png")}></Image>
-      <Text style={styles.text}>Login</Text>
-      <Pressable onPress={() => {router.replace("/(tabs)/");}}>
-        <Text style={styles.text}>Sign in</Text>
+      <Image style={styles.atlasLogo} source={require("../assets/images/logo.png")}></Image>
+      <Text style={styles.loginText}>Login</Text>
+      <TextInput
+        style={styles.inputField}
+        placeholder="Email"
+        placeholderTextColor="white"
+      />
+      <TextInput
+        style={styles.inputField}
+        placeholder="Password"
+        placeholderTextColor="white"
+      />
+      <Pressable onPress={() => { router.replace("/(tabs)/"); }} style={styles.signInButton}>
+        <Text style={styles.buttonText}>Sign in</Text>
       </Pressable>
-      <Link href="/register" replace>
-        <Text style={styles.text}>Create a new account</Text>
+      <Link href="/register" style={styles.newAccountButton} replace>
+        <Text style={styles.buttonText}>Create a new account</Text>
       </Link>
     </View>
   );
@@ -23,14 +33,58 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: '#000435',
+    backgroundColor: '#00003C',
   },
-  logo: {
+  atlasLogo: {
     resizeMode: 'contain',
     width: 250,
     height: 200,
   },
   text: {
     color: 'white',
+    textAlign: 'center',
+  },
+  loginText: {
+    fontSize: 20,
+    color: 'white',
+    fontWeight: 'bold',
+    marginTop: -15,
+    marginBottom: 15
+  },
+  inputField: {
+    width: 400,
+    height: 50,
+    marginBottom: 10,
+    paddingLeft: 10,
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#1DD2AF',
+    color: 'white',
+  },
+  signInButton: {
+    backgroundColor: '#1DD2AF',
+    width: 400,
+    height: 40,
+    borderRadius: 5,
+    marginTop: 15,
+    justifyContent: 'center',
+  },
+  newAccountButton: {
+    borderWidth: 2,
+    borderRadius: 5,
+    width: 400,
+    height: 40,
+    marginTop: 15,
+    borderColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  newAccountText: {
+    color: 'white',
+    textAlign: 'center',
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
   }
 })
