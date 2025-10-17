6190c88aadbf822ff01c373f1877d7ca3656ca35
Zack Yuan <155485510+zack6yuan@users.noreply.github.com>
2025-10-17 11:51:33 -0500

    Initial commit

---
+++ placeholder.tsx
@@ -0,0 +1,156 @@
+export const homeFeed = [
+  {
+    image: "https://placedog.net/400x400?id=1",
+    caption: "This is a caption 1",
+    id: "1",
+    createdBy: "atlas-lumi",
+  },
+  {
+    image: "https://placedog.net/400x400?id=2",
+    caption: "This is a caption 2",
+    id: "2",
+    createdBy: "atlas-lumi",
+  },
+  {
+    image: "https://placedog.net/400x400?id=3",
+    caption: "This is a caption 3",
+    id: "3",
+    createdBy: "atlas-lumi",
+  },
+  {
+    image: "https://placedog.net/400x400?id=4",
+    caption: "This is a caption 4",
+    id: "4",
+    createdBy: "atlas-lumi",
+  },
+  {
+    image: "https://placedog.net/400x400?id=5",
+    caption: "This is a caption 5",
+    id: "5",
+    createdBy: "atlas-lumi",
+  },
+];
+
+export const favoritesFeed = [
+  {
+    image: "https://placedog.net/400x400?id=6",
+    caption: "This is a caption 6",
+    id: "6",
+    createdBy: "atlas-lumi",
+  },
+  {
+    image: "https://placedog.net/400x400?id=7",
+    caption: "This is a caption 7",
+    id: "7",
+    createdBy: "atlas-lumi",
+  },
+  {
+    image: "https://placedog.net/400x400?id=8",
+    caption: "This is a caption 8",
+    id: "8",
+    createdBy: "atlas-lumi",
+  },
+  {
+    image: "https://placedog.net/400x400?id=9",
+    caption: "This is a caption 9",
+    id: "9",
+    createdBy: "atlas-lumi",
+  },
+  {
+    image: "https://placedog.net/400x400?id=10",
+    caption: "This is a caption 10",
+    id: "10",
+    createdBy: "atlas-lumi",
+  },
+];
+
+export const profileFeed = [
+  {
+    image: "https://placedog.net/400x400?id=11",
+    caption: "This is a caption 6",
+    id: "6",
+    createdBy: "atlas-lumi",
+  },
+  {
+    image: "https://placedog.net/400x400?id=12",
+    caption: "This is a caption 7",
+    id: "7",
+    createdBy: "atlas-lumi",
+  },
+  {
+    image: "https://placedog.net/400x400?id=13",
+    caption: "This is a caption 8",
+    id: "8",
+    createdBy: "atlas-lumi",
+  },
+  {
+    image: "https://placedog.net/400x400?id=14",
+    caption: "This is a caption 9",
+    id: "9",
+    createdBy: "atlas-lumi",
+  },
+  {
+    image: "https://placedog.net/400x400?id=15",
+    caption: "This is a caption 10",
+    id: "10",
+    createdBy: "atlas-lumi",
+  },
+  {
+    image: "https://placedog.net/400x400?id=11",
+    caption: "This is a caption 6",
+    id: "6",
+    createdBy: "atlas-lumi",
+  },
+  {
+    image: "https://placedog.net/400x400?id=20",
+    caption: "This is a caption 7",
+    id: "7",
+    createdBy: "atlas-lumi",
+  },
+  {
+    image: "https://placedog.net/400x400?id=19",
+    caption: "This is a caption 8",
+    id: "8",
+    createdBy: "atlas-lumi",
+  },
+  {
+    image: "https://placedog.net/400x400?id=18",
+    caption: "This is a caption 9",
+    id: "9",
+    createdBy: "atlas-lumi",
+  },
+  {
+    image: "https://placedog.net/400x400?id=17",
+    caption: "This is a caption 10",
+    id: "10",
+    createdBy: "atlas-lumi",
+  },
+];
+
+export const userSearch = [
+  {
+    id: "1",
+    username: "atlas-lumi-1",
+    avatar: "https://placedog.net/400x400?id=1",
+  },
+  {
+    id: "2",
+    username: "atlas-lumi-2",
+    avatar: "https://placedog.net/400x400?id=2",
+  },
+  {
+    id: "3",
+    username: "atlas-lumi-3",
+    avatar: "https://placedog.net/400x400?id=3",
+  },
+  {
+    id: "4",
+    username: "atlas-lumi-4",
+    avatar: "https://placedog.net/400x400?id=4",
+  },
+  {
+    id: "5",
+    username: "atlas-lumi-5",
+    avatar: "https://placedog.net/400x400?id=5",
+  },
+];
