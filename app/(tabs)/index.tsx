4499b167bf6f5cc5c83198eadaa6e61566424df0
zack6yuan <zack6yuan@gmail.com>
2025-10-19 22:52:51 -0500

    working on gestures.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,21 +1,55 @@
 import { Image, StyleSheet, Platform, View, Text } from "react-native";
-import { homeFeed } from '../../placeholder'
-
+import { homeFeed } from "../../placeholder";
 import { HelloWave } from "@/components/HelloWave";
 import ParallaxScrollView from "@/components/ParallaxScrollView";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
 
-import { FlashList } from '@shopify/flash-list';
+import {
+  GestureHandlerRootView,
+  GestureDetector,
+  Gesture,
+} from "react-native-gesture-handler";
+
+import Animated, {
+  useSharedValue,
+  withTiming,
+} from "react-native-reanimated";
+
+import { FlashList } from "@shopify/flash-list";
 
 export default function HomeScreen() {
+  const offset = useSharedValue({ x: 0, y: 0 });
+  const popupPosition = useSharedValue({ x: 0, y: 0 });
+  const popupAlpha = useSharedValue(0);
+
+  const longPressGesture = Gesture.LongPress().onStart((_event) => {
+    popupPosition.value = { x: offset.value.x, y: offset.value.y };
+    popupAlpha.value = withTiming(1);
+  });
+
   return (
-    <View>
-      <Image></Image>
-    </View>
-  )
+    <GestureHandlerRootView>
+      <GestureDetector gesture={longPressGesture}>
+        <FlashList
+          data={homeFeed}
+          renderItem={({ item }) => (
+            <View>
+              <Image source={{ uri: item.image }} style={styles.feedImage} />
+            </View>
+          )}
+        />
+      </GestureDetector>
+    </GestureHandlerRootView>
+  );
 }
 
 const styles = StyleSheet.create({
-
+  feedImage: {
+    width: 435,
+    height: 435,
+    marginLeft: 2.5,
+    marginBottom: 10,
+    borderRadius: 20,
+  },
 });
