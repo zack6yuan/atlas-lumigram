e0d0f1c45e6e43b1332ae2a65236d38cedbb8ef4
zack6yuan <zack6yuan@gmail.com>
2025-10-20 21:51:24 -0500

    updated the search page.

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,16 +1,26 @@
 import { Link } from "expo-router";
-import { View, Text } from "react-native";
+import { View, Text, TextInput, StyleSheet } from "react-native";
 
 export default function Search() {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Search Page</Text>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'start'}}>
+      {/* <Text>Search Page</Text>
       <Link href="/profile/1">
         <Text>Profile 1</Text>
       </Link>
       <Link href="/profile/2">
         <Text>Profile 2</Text>
-      </Link>
+      </Link> */}
+      <TextInput style={styles.searchInput} />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  searchInput: {
+    borderWidth: 2,
+    borderColor: '#00D6AD',
+    width: 440,
+    height: 60,
+  },
+});
