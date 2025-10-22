4f20e9089bf6705046e5c8a92c8d6691edbdca13
zack6yuan <zack6yuan@gmail.com>
2025-10-22 09:27:37 -0500

    add post page is updated.

--- app/(tabs)/addPost.tsx
+++ app/(tabs)/addPost.tsx
@@ -1,40 +1,58 @@
 import { useImagePicker } from "@/hooks/useImagePicker";
 import { useState } from "react";
-import { View, Text, Button, StyleSheet, Image, TextInput, Pressable } from "react-native";
-const placeHolder = require('../../assets/images/placeholder.png')
+import {
+  View,
+  Text,
+  Button,
+  StyleSheet,
+  Image,
+  TextInput,
+  Pressable,
+} from "react-native";
+const placeHolder = require("../../assets/images/placeholder.png");
 
 export default function addPost() {
   const [caption, setCaption] = useState("");
   const [loading, setLoading] = useState(false);
   const { image, openImagePicker, reset } = useImagePicker();
 
+  // if no image is selected, show the placeholder
+  const newSource = image ? { uri: image } : placeHolder;
+
   return (
     <View style={styles.container}>
-      <Image
-        source={placeHolder}
-        style={styles.placeholder}
-      />
-      <TextInput
-        placeholder="Add a caption"
-        placeholderTextColor={'#666666'}
-        style={styles.imageCaption}
-      />
-      <Pressable style={styles.saveButton}>
-        <Text style={styles.saveText}>
-          Save
-        </Text>
-      </Pressable>
-      <Pressable style={styles.resetButton}>
-        <Text>Reset</Text>
-      </Pressable>
+      <Image source={newSource} style={styles.placeholder} />
+      {/* Case: No image is currently selected */}
+      {!image && (
+        <Pressable style={styles.saveButton} onPress={openImagePicker}>
+          <Text style={styles.saveText}>Choose a photo</Text>
+        </Pressable>
+      )}
+      {/* Case: Image is currently selected */}
+      {image && (
+        <View>
+          <TextInput
+            placeholder="Add a caption"
+            placeholderTextColor={"#666666"}
+            style={styles.imageCaption}
+          />
+          <Pressable style={styles.saveImageButton}>
+            <Text style={styles.saveText}>Save</Text>
+          </Pressable>
+          <Pressable style={styles.reset} onPress={reset}>
+            <Text>Reset</Text>
+          </Pressable>
+        </View>
+      )}
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
-    alignItems: 'center',
-    justifyContent: 'center'
+    width: '100%',
+    alignItems: "center",
+    justifyContent: "center",
   },
   placeholder: {
     width: 370,
@@ -45,25 +63,36 @@ const styles = StyleSheet.create({
   imageCaption: {
     borderWidth: 2,
     borderRadius: 5,
-    borderColor: '#00D6AD',
+    borderColor: "#00D6AD",
     width: 400,
     height: 50,
     marginTop: 30,
     paddingLeft: 20,
   },
   saveButton: {
-    backgroundColor: '#00D6AD',
+    backgroundColor: "#00D6AD",
     borderRadius: 10,
-    width: 350,
+    width: 365,
     height: 60,
-    marginTop: 20,
+    marginTop: 35,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  saveImageButton: {
+    backgroundColor: "#00D6AD",
+    borderRadius: 10,
+    marginTop: 25,
+    marginLeft: 17,
+    width: 365,
+    height: 60,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
   saveText: {
-    color: 'white',
+    color: "white",
   },
-  resetButton: {
-    marginTop: 50,
+  reset: {
+    alignItems: 'center',
+    marginTop: 40,
   }
-})
+});
--- hooks/useImagePicker.ts
+++ hooks/useImagePicker.ts
@@ -1,14 +1,31 @@
 import { useState } from 'react';
+import { usePermissions } from 'expo-media-library';
+import * as ImagePicker from 'expo-image-picker';
+import { ImageBase } from 'react-native';
 
 export function useImagePicker() {
-  const [image, setImage] = useState(undefined)
+  const [image, setImage] = useState<string | undefined>(undefined)
+  const [status, requestPermission] = usePermissions();
 
-  function openImagePicker(){
-    alert("Open Image Picker")
+  async function openImagePicker(){
+    if (status === null) {
+      const permission = await requestPermission();
+      if (permission.granted === false) {
+        alert('You need to grant permissions to access your camera roll');
+        return;
+      }
+    }
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+    });
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
   }
 
   function reset() {
-    alert("reset")
+    alert("Reset Clicked")
   }
 
   return {image, openImagePicker, reset}
