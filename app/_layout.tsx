eaaef0d32e454e5ce3142d96f443ee743e83de37
zack6yuan <zack6yuan@gmail.com>
2025-10-22 09:42:59 -0500

    hid the header for the edit profile page and adjusted the margin.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout() {
           <Stack.Screen name="register" options={{ headerShown: false }} />
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
           <Stack.Screen name="editProfile" options={{
-            headerShown: true,
+            headerShown: false,
             headerTitleAlign: 'left',
           }}
           />
--- app/editProfile.tsx
+++ app/editProfile.tsx
@@ -33,7 +33,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    marginTop: 40,
+    marginTop: 140,
   },
   userImage: {
     width: 100,
