a31ba71e238907583caf064f6a28d8549e3e8adf
zack6yuan <zack6yuan@gmail.com>
2025-10-27 12:06:10 -0500

    working.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,18 +1,30 @@
-import { Alert, Image, StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 import { homeFeed } from "../../placeholder";
 
 import { Gesture, GestureDetector } from "react-native-gesture-handler";
 
 import { runOnJS } from "react-native-reanimated";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-import { FlashList } from "@shopify/flash-list";
 import { useAuth } from "@/components/AuthProvider";
+import { FlashList } from "@shopify/flash-list";
+
+import { db } from "@/firebaseConfig";
+import { collection, query, where, getDocs, snapshotEqual } from "firebase/firestore";
+import { getDownloadURL, getStorage, ref } from "firebase/storage";
+import { getAdditionalUserInfo } from "firebase/auth";
 
 export default function HomeScreen() {
   const auth = useAuth();
   const [pressed, setPressed] = useState<boolean>(false);
+
+  // Get the images from FireStore
+  const [url, setUrl] = useState();
+
+  function getPhotos() {
+    
+  }
 
   const longPress = Gesture.LongPress()
     // Beginning of the gesture
@@ -35,27 +47,29 @@ export default function HomeScreen() {
       runOnJS(displayAlert)();
   })
 
-  const bothGestures = Gesture.Race(doubleTap, longPress)
+  const gestureRace = Gesture.Race(doubleTap, longPress)
 
   return (
-    <FlashList
-      data={homeFeed}
-      renderItem={({ item }) => (
-        <GestureDetector gesture={bothGestures}>
-          <View>
-            <Text>Welcome {auth.user?.email}!</Text>
-            <Image source={{ uri: item.image }} style={styles.feedImage} />
-            {pressed && (
-              <View style={styles.overlayContainer}>
-                <Text style={styles.overlayText}>
-                  Pariatur officia ut dolor commodo. Adipisicing reprehenderit magna dolor non fugiat ea fugiat ea sunt duis nostrud reprehenderit cupidatat magna.
-                </Text>
-              </View>
-            )}
-          </View>
-        </GestureDetector>
-      )}
-    />
+    // <FlashList
+    //   data={homeFeed}
+    //   renderItem={({  }) => (
+    //     <GestureDetector gesture={gestureRace}>
+    //       <View>
+    //         <Text>Welcome {auth.user?.email}!</Text>
+    //         <Image source={{ uri: url }} style={styles.feedImage} />
+    //         {pressed && (
+    //           <View style={styles.overlayContainer}>
+    //             <Text style={styles.overlayText}>
+    //               Pariatur officia ut dolor commodo. Adipisicing reprehenderit magna dolor non fugiat ea fugiat ea sunt duis nostrud reprehenderit cupidatat magna.
+    //             </Text>
+    //           </View>
+    //         )}
+    //       </View>
+    //     </GestureDetector>
+    //   )}
+    // />
+    <Image source={url}>
+    </Image>
   );
 }
 
